a028d3e598d4f20c7a79940973e4f759d2563b1b
Vengatesh1503 <vp8179@srmist.edu.in>
2026-04-06 15:07:30 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Simple Java application to display "Hello World" on the console.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Output the text to the console
+        System.out.println("Hello World");
+    }
+}
